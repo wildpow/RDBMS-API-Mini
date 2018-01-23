353dde12fbb39a7bef1568d38f5047ee201f19d9
Aaron Wilder <element1028@gmail.com>
2018-01-23 07:25:30 -0800

    stuff

--- server.js
+++ server.js
@@ -1,11 +1,29 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const knex = require('./database/db.js')
 const server = express();
 
 server.use(bodyParser.json());
 
 // endpoints here
+server.get('/', function(req, res) {
+    res.status(200).json({ success: true });
+});
+
+server.post('/api/zoos', function(req, res) {
+    const zoo = req.body;
+
+    knex
+        .insert(zoo)
+        .into('zoos')
+        .then(function(ids) {
+            res.status(201).json({ ids });
+        })
+        .catch(function() {
+            res.status(500).json({ errorMessage: 'Could not inser the Zoo' });
+        });
+});
 
 const port = 3000;
 server.listen(port, function() {
